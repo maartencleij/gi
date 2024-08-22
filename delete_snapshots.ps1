b79dae116d50a27aa2a174d72dacdef276597cfc
Maarten Cleij <maarten.cleij@simac.com>
2024-08-22 15:10:47 +0200

    paralel

--- delete_snapshots.ps1
+++ delete_snapshots.ps1
@@ -4,56 +4,66 @@ $ErrorActionPreference = "Stop"
 # Load VM names and other variables from CSV file
 $vmList = Import-Csv -Path "vmconfig.csv"
 
+$jobs = @()
+
 foreach ($vm in $vmList) {
-    $VMname = $vm.VMName
-    $ResourceGroupName = $vm.ResourceGroupName
-    $AzTenantId = $vm.AzTenantId
-    $AzSubscriptionId = $vm.AzSubscriptionId
-    $TOPdeskActivity = $vm.TOPdeskActivity
-    $TopdeskActivityWithoutSpaces = $TOPdeskActivity.Replace(" ", "")
+    $jobs += Start-Job -ScriptBlock {
+        $VMname = $using:vm.VMName
+        $ResourceGroupName = $using:vm.ResourceGroupName
+        $AzTenantId = $using:vm.AzTenantId
+        $AzSubscriptionId = $using:vm.AzSubscriptionId
+        $TOPdeskActivity = $using:vm.TOPdeskActivity
+        $TopdeskActivityWithoutSpaces = $TOPdeskActivity.Replace(" ", "")
 
-    try {
-        Connect-AzAccount -TenantId $AzTenantId
-        Select-AzSubscription $AzSubscriptionId
+        try {
+            Connect-AzAccount -TenantId $AzTenantId
+            Select-AzSubscription $AzSubscriptionId
 
-        # Get the VM details
-        $Vm2Snapshot = Get-AzVM -Name $VMname -ResourceGroupName $ResourceGroupName
-        if ($null -ne $Vm2Snapshot) {
+            # Get the VM details
+            $Vm2Snapshot = Get-AzVM -Name $VMname -ResourceGroupName $ResourceGroupName
+            if ($null -ne $Vm2Snapshot) {
 
-            # Generate snapshot names using the same convention as the snapshot creation script
-            $OsDiskSnapshotName = "$($Vm2Snapshot.StorageProfile.OsDisk.Name)-OS-$($TopdeskActivityWithoutSpaces)"
+                # Generate snapshot names using the same convention as the snapshot creation script
+                $OsDiskSnapshotName = "$($Vm2Snapshot.StorageProfile.OsDisk.Name)-OS-$($TopdeskActivityWithoutSpaces)"
 
-            # Find and remove the OS disk snapshot if it includes the TOPdeskActivity identifier
-            $OsDiskSnapshot = Get-AzSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $OsDiskSnapshotName -ErrorAction SilentlyContinue
-            if ($null -ne $OsDiskSnapshot -and $OsDiskSnapshot.Name -like "*$TopdeskActivityWithoutSpaces*") {
-                Remove-AzSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $OsDiskSnapshotName -Force
-                Write-Output ("Deleted snapshot [ $OsDiskSnapshotName ] for OS disk of VM [ $VMname ] in resource group [ $ResourceGroupName ]")
-            } else {
-                Write-Output ("No matching OS disk snapshot found for VM [ $VMname ] with snapshot name [ $OsDiskSnapshotName ] including TOPdeskActivity [ $TopdeskActivity ]")
-            }
-
-            # Check for and remove data disk snapshots if they include the TOPdeskActivity identifier
-            $Vm2SnapshotDataDisks = $Vm2Snapshot.StorageProfile.DataDisks
-            if ($Vm2SnapshotDataDisks.count -gt 0) {
-                foreach ($DataDisk in $Vm2SnapshotDataDisks) {
-                    $DataDiskSnapshotName = "$($DataDisk.Name)-LUN_$($DataDisk.Lun)-$($TopdeskActivityWithoutSpaces)"
-
-                    $DataDiskSnapshot = Get-AzSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $DataDiskSnapshotName -ErrorAction SilentlyContinue
-                    if ($null -ne $DataDiskSnapshot -and $DataDiskSnapshot.Name -like "*$TopdeskActivityWithoutSpaces*") {
-                        Remove-AzSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $DataDiskSnapshotName -Force
-                        Write-Output ("Deleted snapshot [ $DataDiskSnapshotName ] for data disk [ $DataDisk.Name ] of VM [ $VMname ] in resource group [ $ResourceGroupName ]")
-                    } else {
-                        Write-Output ("No matching data disk snapshot found for VM [ $VMname ] with snapshot name [ $DataDiskSnapshotName ] including TOPdeskActivity [ $TopdeskActivity ]")
-                    }
+                # Find and remove the OS disk snapshot if it includes the TOPdeskActivity identifier
+                $OsDiskSnapshot = Get-AzSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $OsDiskSnapshotName -ErrorAction SilentlyContinue
+                if ($null -ne $OsDiskSnapshot -and $OsDiskSnapshot.Name -like "*$TopdeskActivityWithoutSpaces*") {
+                    Remove-AzSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $OsDiskSnapshotName -Force
+                    Write-Output ("Deleted snapshot [ $OsDiskSnapshotName ] for OS disk of VM [ $VMname ] in resource group [ $ResourceGroupName ]")
+                } else {
+                    Write-Output ("No matching OS disk snapshot found for VM [ $VMname ] with snapshot name [ $OsDiskSnapshotName ] including TOPdeskActivity [ $TopdeskActivity ]")
                 }
-            } else {
-                Write-Output ("No data disks found for VM [ $VMname ] in [ $ResourceGroupName ]")
-            }
 
-        } else {
-            Write-Error ("Unable to find VM [ $($VMname) ] in [ $($ResourceGroupName) ] in subscription [$($AzSubscriptionId)]. Please check input and try again")
+                # Check for and remove data disk snapshots if they include the TOPdeskActivity identifier
+                $Vm2SnapshotDataDisks = $Vm2Snapshot.StorageProfile.DataDisks
+                if ($Vm2SnapshotDataDisks.count -gt 0) {
+                    foreach ($DataDisk in $Vm2SnapshotDataDisks) {
+                        $DataDiskSnapshotName = "$($DataDisk.Name)-LUN_$($DataDisk.Lun)-$($TopdeskActivityWithoutSpaces)"
+
+                        $DataDiskSnapshot = Get-AzSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $DataDiskSnapshotName -ErrorAction SilentlyContinue
+                        if ($null -ne $DataDiskSnapshot -and $DataDiskSnapshot.Name -like "*$TopdeskActivityWithoutSpaces*") {
+                            Remove-AzSnapshot -ResourceGroupName $ResourceGroupName -SnapshotName $DataDiskSnapshotName -Force
+                            Write-Output ("Deleted snapshot [ $DataDiskSnapshotName ] for data disk [ $DataDisk.Name ] of VM [ $VMname ] in resource group [ $ResourceGroupName ]")
+                        } else {
+                            Write-Output ("No matching data disk snapshot found for VM [ $VMname ] with snapshot name [ $DataDiskSnapshotName ] including TOPdeskActivity [ $TOPdeskActivity ]")
+                        }
+                    }
+                } else {
+                    Write-Output ("No data disks found for VM [ $VMname ] in [ $ResourceGroupName ]")
+                }
+
+            } else {
+                Write-Error ("Unable to find VM [ $($VMname) ] in [ $($ResourceGroupName) ] in subscription [$($AzSubscriptionId)]. Please check input and try again")
+            }
+        } catch {
+            Write-Error ("Error thrown while logging into Azure or executing part of the procedure: $($_.Exception.Message)")
         }
-    } catch {
-        Write-Error ("Error thrown while logging into Azure or executing part of the procedure: $($_.Exception.Message)")
     }
 }
+
+# Wait for all jobs to complete
+$jobs | ForEach-Object { $_ | Wait-Job | Receive-Job }
+
+# Clean up
+$jobs | ForEach-Object { Remove-Job $_ }
--- snapshot.ps1
+++ snapshot.ps1
@@ -5,58 +5,68 @@ $StartVmAfterCompletion = $true
 # Load VM names and other variables from CSV file
 $vmList = Import-Csv -Path "vmconfig.csv"
 
+$jobs = @()
+
 foreach ($vm in $vmList) {
-    $VMname = $vm.VMName
-    $ResourceGroupName = $vm.ResourceGroupName
-    $AzTenantId = $vm.AzTenantId
-    $AzSubscriptionId = $vm.AzSubscriptionId
-    $TOPdeskActivity = $vm.TOPdeskActivity
-    $TopdeskActivityWithoutSpaces = $TOPdeskActivity.Replace(" ", "")
+    $jobs += Start-Job -ScriptBlock {
+        $VMname = $using:vm.VMName
+        $ResourceGroupName = $using:vm.ResourceGroupName
+        $AzTenantId = $using:vm.AzTenantId
+        $AzSubscriptionId = $using:vm.AzSubscriptionId
+        $TOPdeskActivity = $using:vm.TOPdeskActivity
+        $TopdeskActivityWithoutSpaces = $TOPdeskActivity.Replace(" ", "")
 
-    try {
-        Connect-AzAccount -TenantId $AzTenantId
-        Select-AzSubscription $AzSubscriptionId
+        try {
+            Connect-AzAccount -TenantId $AzTenantId
+            Select-AzSubscription $AzSubscriptionId
 
-        # Evaluate if VM exists before continuing
-        $Vm2Snapshot = Get-AzVM -Name $VMname -ResourceGroupName $ResourceGroupName
-        if ($null -ne $Vm2Snapshot) {
-            # Shutdown VM
-            Write-Output ("Initiating shutdown of VM [ $($VmName) ] in [ $($ResourceGroupName) ]")
-            Stop-AzVM -Name $Vm2Snapshot.Name -ResourceGroupName $Vm2Snapshot.ResourceGroupName -Force
-            Write-Output ("VM shut down successfully")
+            # Evaluate if VM exists before continuing
+            $Vm2Snapshot = Get-AzVM -Name $VMname -ResourceGroupName $ResourceGroupName
+            if ($null -ne $Vm2Snapshot) {
+                # Shutdown VM
+                Write-Output ("Initiating shutdown of VM [ $($VMname) ] in [ $($ResourceGroupName) ]")
+                Stop-AzVM -Name $Vm2Snapshot.Name -ResourceGroupName $Vm2Snapshot.ResourceGroupName -Force
+                Write-Output ("VM shut down successfully")
 
-            # Snapshot VM OS disk
-            $OsDiskSnapshotName = "$($Vm2Snapshot.StorageProfile.OsDisk.Name)-OS-$($TopdeskActivityWithoutSpaces)"
-            $OsDiskSnapShotConfig = New-AzSnapshotConfig -SourceUri $Vm2Snapshot.StorageProfile.OsDisk.ManagedDisk.Id -CreateOption Copy -Location $Vm2Snapshot.Location
-            New-AzSnapshot -Snapshot $OsDiskSnapShotConfig -SnapshotName $OsDiskSnapshotName -ResourceGroupName $Vm2Snapshot.ResourceGroupName
-            Write-Output ("Creating snapshot from [ $($Vm2Snapshot.StorageProfile.OsDisk.Name) ] completed successfully")
+                # Snapshot VM OS disk
+                $OsDiskSnapshotName = "$($Vm2Snapshot.StorageProfile.OsDisk.Name)-OS-$($TopdeskActivityWithoutSpaces)"
+                $OsDiskSnapShotConfig = New-AzSnapshotConfig -SourceUri $Vm2Snapshot.StorageProfile.OsDisk.ManagedDisk.Id -CreateOption Copy -Location $Vm2Snapshot.Location
+                New-AzSnapshot -Snapshot $OsDiskSnapShotConfig -SnapshotName $OsDiskSnapshotName -ResourceGroupName $Vm2Snapshot.ResourceGroupName
+                Write-Output ("Creating snapshot from [ $($Vm2Snapshot.StorageProfile.OsDisk.Name) ] completed successfully")
 
-            # Evaluate if server has data disks, if so snapshot them
-            $Vm2SnapshotDataDisks = $Vm2Snapshot.StorageProfile.DataDisks
-            if ($Vm2SnapshotDataDisks.count -gt 0) {
-                foreach ($DataDisk in $Vm2SnapshotDataDisks) {
-                    $DataDiskSnapshotName = "$($DataDisk.Name)-LUN_$($DataDisk.Lun)-$($TopdeskActivityWithoutSpaces)"
-                    $DataDiskSnapshotUri = (Get-AzDisk -DiskName $DataDisk.Name -ResourceGroupName $Vm2Snapshot.ResourceGroupName).Id
-                    $DataDiskSnapShotConfig = New-AzSnapshotConfig -SourceUri $DataDiskSnapshotUri -CreateOption Copy -Location $Vm2Snapshot.Location
-                    New-AzSnapshot -Snapshot $DataDiskSnapShotConfig -SnapshotName $DataDiskSnapshotName -ResourceGroupName $Vm2Snapshot.ResourceGroupName
-                    Write-Output ("Creating snapshot from [ $($DataDisk.Name) ] completed successfully")
+                # Evaluate if server has data disks, if so snapshot them
+                $Vm2SnapshotDataDisks = $Vm2Snapshot.StorageProfile.DataDisks
+                if ($Vm2SnapshotDataDisks.count -gt 0) {
+                    foreach ($DataDisk in $Vm2SnapshotDataDisks) {
+                        $DataDiskSnapshotName = "$($DataDisk.Name)-LUN_$($DataDisk.Lun)-$($TopdeskActivityWithoutSpaces)"
+                        $DataDiskSnapshotUri = (Get-AzDisk -DiskName $DataDisk.Name -ResourceGroupName $Vm2Snapshot.ResourceGroupName).Id
+                        $DataDiskSnapShotConfig = New-AzSnapshotConfig -SourceUri $DataDiskSnapshotUri -CreateOption Copy -Location $Vm2Snapshot.Location
+                        New-AzSnapshot -Snapshot $DataDiskSnapShotConfig -SnapshotName $DataDiskSnapshotName -ResourceGroupName $Vm2Snapshot.ResourceGroupName
+                        Write-Output ("Creating snapshot from [ $($DataDisk.Name) ] completed successfully")
+                    }
+                } else {
+                    Write-Output ("No data disks found for VM [ $($VMname) ] in [ $($ResourceGroupName) ]")
+                }
+
+                # Evaluate if VM needs to be started following completion, if so start it.
+                if ($using:StartVmAfterCompletion -eq $true) {
+                    Write-Output ("Initiating startup of VM [ $($VMname) ] in [ $($ResourceGroupName) ]")
+                    Start-AzVM -Name $Vm2Snapshot.Name -ResourceGroupName $Vm2Snapshot.ResourceGroupName
+                    Write-Output ("VM started successfully")
+                } else {
+                    Write-Output ("StartVmAfterCompletion set to false. Skipping start of VM.")
                 }
             } else {
-                Write-Output ("No data disks found for VM [ $($VmName) ] in [ $($ResourceGroupName) ]")
+                Write-Error ("Unable to find VM [ $($VMname) ] in [ $($ResourceGroupName) ] in subscription [$($AzSubscriptionId)]. Please check input and try again")
             }
-
-            # Evaluate if VM needs to be started following completion, if so start it.
-            if ($StartVmAfterCompletion -eq $true) {
-                Write-Output ("Initiating startup of VM [ $($VmName) ] in [ $($ResourceGroupName) ]")
-                Start-AzVM -Name $Vm2Snapshot.Name -ResourceGroupName $Vm2Snapshot.ResourceGroupName
-                Write-Output ("VM started successfully")
-            } else {
-                Write-Output ("StartVmAfterCompletion set to false. Skipping start of VM.")
-            }
-        } else {
-            Write-Error ("Unable to find VM [ $($VmName) ] in [ $($ResourceGroupName) ] in subscription [$($AzSubscriptionId)]. Please check input and try again")
+        } catch {
+            Write-Error ("Error thrown while logging into Azure or executing part of the procedure: $($_.Exception.Message)")
         }
-    } catch {
-        Write-Error ("Error thrown while logging into Azure or executing part of the procedure: $($_.Exception.Message)")
     }
 }
+
+# Wait for all jobs to complete
+$jobs | ForEach-Object { $_ | Wait-Job | Receive-Job }
+
+# Clean up
+$jobs | ForEach-Object { Remove-Job $_ }
